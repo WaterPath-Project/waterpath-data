f543ed15cf66ef37369e7c586d4657006837b7c3
Panagis Katsivelis <panagis.katsivelis@venthic.com>
2026-02-07 22:49:13 +0200

    decimal points reduction

--- world_admin_units_urbanisation_degree/scripts/prepare.py
+++ world_admin_units_urbanisation_degree/scripts/prepare.py
@@ -11,11 +11,13 @@ def roundPop(x):
     else:
         return 0
 
-if not os.path.isfile("ghc.zip"):
-    urllib.request.urlretrieve("https://jeodpp.jrc.ec.europa.eu/ftp/jrc-opendata/GHSL//GHS_DUC_GLOBE_R2023A/V2-0/GHS_DUC_MT_GLOBE_R2023A_V2_0.zip", "ghc.zip")
+fullfilename = os.path.join("../data/original", "ghc.zip")
+
+if not os.path.isfile(fullfilename):
+    urllib.request.urlretrieve("https://jeodpp.jrc.ec.europa.eu/ftp/jrc-opendata/GHSL//GHS_DUC_GLOBE_R2023A/V2-0/GHS_DUC_MT_GLOBE_R2023A_V2_0.zip", fullfilename)
 
 if not os.path.isfile("../data/GHS_DUC_GLOBE_R2023A_V2_0.xlsx"):
-    with zipfile.ZipFile("ghc.zip", 'r') as zip_ref:
+    with zipfile.ZipFile(fullfilename, 'r') as zip_ref:
         zip_ref.extractall("../data")
 
 years = [ 2025 ]
@@ -29,8 +31,9 @@ for filename in os.listdir("../data"):
             newdf = df.iloc[:, [0, 1, 2, 8]]
             newdf.rename(columns={ newdf.columns[0]: "gid", newdf.columns[1]: "alpha3", newdf.columns[2]: "totalPopulation", newdf.columns[3]: "fractionUrban" }, inplace = True)
             newdf['totalPopulation'] = newdf['totalPopulation'].apply(lambda x: roundPop(x))
+            newdf['fractionUrban'] = newdf['fractionUrban'].astype(float).round(3)
             newdf.to_csv('../data/world_urbanisation_level'+level+'.csv', index=False)              
 
 for filename in glob.glob("../data/GHS*"):
     os.remove(filename) 
-os.remove("ghc.zip") 
+os.remove(fullfilename) 
