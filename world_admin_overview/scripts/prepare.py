881119b976dd869a140661c50dbf9ba9e5ee85fe
katsivelisp <pankaper@gmail.com>
2026-03-16 16:42:13 +0200

    UNSD countries + FAOSTAT (2024)

--- world_admin_overview/scripts/prepare.py
+++ world_admin_overview/scripts/prepare.py
@@ -2,11 +2,11 @@ import pygadm
 import pandas as pd
 import urllib.request, json 
 
-data = pd.read_csv('https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/refs/heads/master/all/all.csv', encoding='utf-8')
+data = pd.read_csv('../../unsd_countries/data/unsd_countries.csv', encoding='utf-8', sep=';')
 
 gdf = pygadm.Names()
 
-merged = pd.merge(gdf, data, left_on='GID_0', right_on='alpha-3').drop(['name','alpha-3','region-code','sub-region-code','intermediate-region-code'], axis=1).rename(columns={"alpha-2": "ALPHA_2", "region": "REGION", "sub-region": "SUB_REGION", "intermediate-region": "INTERMEDIATE_REGION", "country-code": "COUNTRY_CODE"}).set_index('iso_3166-2')
+merged = pd.merge(gdf, data, left_on='GID_0', right_on='ISO-alpha3 Code').drop(['NAME_0', 'ISO-alpha3 Code', 'Global Code', 'Global Name', 'Region Code', 'Sub-region Code', 'Intermediate Region Code', 'Least Developed Countries (LDC)', 'Land Locked Developing Countries (LLDC)', 'Small Island Developing States (SIDS)'], axis=1).rename(columns={'Country or Area': 'NAME_0', 'ISO-alpha2 Code': 'ALPHA_2', 'M49 Code': 'COUNTRY_CODE', 'Region Name': 'REGION', 'Sub-region Name': 'SUB_REGION', 'Intermediate Region Name': 'INTERMEDIATE_REGION'}).set_index('iso_3166-2')
 
 for i, row in merged.iterrows():
     sub = pygadm.Names(admin=row['GID_0'], content_level=5)
